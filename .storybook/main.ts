36a4fb7ed94e969654622cde3592695ed85c34c3
Gustavo Henrique <gustavo@netvmi.com>
2025-01-16 16:13:46 -0300

    chore: add missing addons in the addon list storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.{ts,tsx}'],
   staticDirs: ['../public', '../src/assets'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-themes',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
